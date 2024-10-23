67796acc9b0a3bcea327b0f1f2368abcf08a3916
BEATRIZ GARRIDO JARAMILLO <bgarrjar@myuax.com>
2024-10-23 18:02:18 +0200

    Ejercicio 2

--- proyecto1/main.cpp
+++ proyecto1/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+#include "Estudiante.h"
+#include "Persona.h"
+
 int suma(int a, int b) {
     return a + b;
 }
@@ -12,5 +15,21 @@ int main() {
     std::cout << "Valor de g antes de la función suma: " << g << std::endl;
     std::cout << "La suma es: " << suma(5, g) << std::endl;
     std::cout << "Valor de g después de la función suma: " << g << std::endl;
+
+    Persona persona1;
+    persona1.setNombre("Clara");
+    persona1.setEdad(23);
+    persona1.setGenero("Femenino");
+    std::cout << "Nombre:" << persona1.getNombre() << std::endl;
+    std::cout << "Edad:" << persona1.getEdad() << std::endl;
+    std::cout << "Género:" << persona1.getGenero() << std::endl;
+
+    Estudiante estudiante1;
+    estudiante1.setNombre("Jorge");
+    estudiante1.setEdad(25);
+    estudiante1.setGenero("Masculino");
+    estudiante1.setGrado("Segundo Año");
+    estudiante1.mostrarDetalles();
+
     return 0;
 }
